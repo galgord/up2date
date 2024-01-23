e91d94227d883a549e0fa4c89a48607bd663ed99
Gal Gordon <gal.gordon@gmail.com>
2024-01-23 08:34:01 -0300

    added expo router and supabae

---
+++ src/app/home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const Home: React.FC = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({});
+export default Home
